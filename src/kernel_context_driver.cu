63eb6551e996c66c360b6a01d584b6ceeb46f387
daltonrpruitt <dp987@msstate.edu>
2022-02-07 16:34:20 -0600

    Create and pass device context to driver obj init
    In actual main()

--- src/kernel_context_driver.cu
+++ src/kernel_context_driver.cu
@@ -27,9 +27,10 @@ int main() {
     cout << "Processing " << N * sizeof(vt) / 1024 * 2 << " KB of data (1/2 reads; 1/2 writes)" << endl;
     typedef ArrayCopyContext<vt, int> kernel_t;
     typedef MicrobenchmarkDriver<kernel_t> driver_t;
+    device_context dev_ctx;
 
     std::vector<int> bs_vec; for (int bs = 32; bs <= 1024; bs *= 2) { bs_vec.push_back(bs);}
-    driver_t driver(N, bs_vec, "../../output/kernel_output.csv");
+    driver_t driver(N, bs_vec, "../../output/kernel_output.csv", dev_ctx);
     driver.check_then_run_kernels();
 
     return 0;
