e06804d4bf1182606ce4f6b103cc87acf0578827
daltonrpruitt <dp987@msstate.edu>
2022-02-10 16:55:21 -0600

    Make use of the new occupancy spanning option

--- src/kernel_context_driver.cu
+++ src/kernel_context_driver.cu
@@ -33,14 +33,17 @@ int main() {
     device_context dev_ctx;
     if(!dev_ctx.init()) return -1;
 
-    std::vector<int> bs_vec; for (int bs = 32; bs <= 1024; bs *= 2) { bs_vec.push_back(bs);}
-    
-    copy_driver_t copy_driver(N, bs_vec, "../../output/copy_kernel_output.csv", dev_ctx);
+    std::vector<int> bs_vec;
+    // Only one of the next two lines 
+    // for (int bs = 32; bs <= 1024; bs *= 2) { bs_vec.push_back(bs);}
+    bs_vec.push_back(128);
+
+    copy_driver_t copy_driver(N, bs_vec, "../../output/copy_kernel_output.csv", dev_ctx, true);
     copy_driver.check_then_run_kernels();
     
-    indirection_driver_direct_t direct_driver(N, bs_vec, "../../output/direct_kernel_output.csv", dev_ctx);
+    indirection_driver_direct_t direct_driver(N, bs_vec, "../../output/direct_kernel_output.csv", dev_ctx, true);
     direct_driver.check_then_run_kernels();
-    indirection_driver_indirect_t indirect_driver(N, bs_vec, "../../output/indirect_kernel_output.csv", dev_ctx);
+    indirection_driver_indirect_t indirect_driver(N, bs_vec, "../../output/indirect_kernel_output.csv", dev_ctx, true);
     indirect_driver.check_then_run_kernels();
 
     return 0;
