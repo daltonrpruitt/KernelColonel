710514a8b2951ec0ed8e34dd973e38e2b07fe798
daltonrpruitt <dp987@msstate.edu>
2022-02-15 09:48:21 -0600

    Update comp_intens kernel
    Try to have computational intensity be main impact on performance.
    Not sure if works...

--- src/kernels/computation.cu
+++ src/kernels/computation.cu
@@ -2,7 +2,7 @@
 /**
  * @file computation.cu
  * @author Dalton Winans-Pruitt (daltonrpruitt@gmail.com)
- * @brief Derived from KernelCPUContext; varied computational intensity
+ * @brief Vary computational intensity
  * @version 0.1
  * @date 2022-02-14
  * 
@@ -21,22 +21,35 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-template<typename vt, typename it>
+#define LOOPS 100
+template<typename vt, typename it, int computational_intensity>
 __forceinline__ __host__ __device__        
 void kernel(uint idx, vt* gpu_in, vt* gpu_out){
-        gpu_out[idx] = gpu_in[idx];
+    // float tmp = float(idx);
+    vt data = gpu_in[idx];
+// #pragma unroll
+    for (int i = 0; i < LOOPS; i++){
+// #pragma unroll
+        for (int j = 0; j < computational_intensity-1; j++){ // -1 since will subtract
+            data += vt(1); 
+        }
+        data -= vt(computational_intensity-1);
+        data = gpu_in[uint(data)];
+    }
+
+    gpu_out[int(data)] = data;
 }
 
 
-template<typename vt, typename it>
+template<typename vt, typename it, int computational_intensity>
 __global__        
-void kernel_for_regs(uint idx, vt* gpu_in, vt* gpu_out){
+void comp_intens_kernel_for_regs(uint idx, vt* gpu_in, vt* gpu_out){
         extern __shared__ int dummy[];
-        kernel<vt, it>(idx, gpu_in, gpu_out);
+        kernel<vt, it, computational_intensity>(idx, gpu_in, gpu_out);
 }
 
-template<typename vt, typename it>
-struct ArrayCopyContext : public KernelCPUContext<vt, it> {
+template<typename vt, typename it, int computational_intensity>
+struct ComputationalIntensityContext : public KernelCPUContext<vt, it> {
     public:
         typedef KernelCPUContext<vt, it> super;
         // name = "Array_Copy";
@@ -49,7 +62,7 @@ struct ArrayCopyContext : public KernelCPUContext<vt, it> {
         vt* & d_in = super::device_data_ptrs[0];
         vt* & d_out = super::device_data_ptrs[1];
 
-        int reads_per_element = 1;
+        int reads_per_element = 1 + LOOPS;
         int writes_per_element = 1;
         int total_reads;
         int total_writes;
@@ -64,13 +77,13 @@ struct ArrayCopyContext : public KernelCPUContext<vt, it> {
             }
         } ctx ;
 
-        ArrayCopyContext(int n, int bs, device_context dev_ctx, int shd_mem_alloc=0) 
+        ComputationalIntensityContext(int n, int bs, device_context dev_ctx, int shd_mem_alloc=0) 
             : super(1, 1, 0, n, bs, dev_ctx, shd_mem_alloc) {
-            this->name = "ArrayCopy"; // _N=" +std::to_string(n) + "_Bs="+std::to_string(bs);
+            this->name = "ComputationalIntesity"; // _N=" +std::to_string(n) + "_Bs="+std::to_string(bs);
             total_reads = N * reads_per_element;
             total_writes = N * writes_per_element;
         }
-        ~ArrayCopyContext(){}
+        ~ComputationalIntensityContext(){}
 
         void init_inputs(bool& pass) override {
             for(int i=0; i<N; ++i){
@@ -104,7 +117,7 @@ struct ArrayCopyContext : public KernelCPUContext<vt, it> {
         void local_compute_register_usage(bool& pass) override {   
             // Kernel Registers 
             struct cudaFuncAttributes funcAttrib;
-            cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *kernel_for_regs<vt,it>), "getting function attributes (for # registers)", pass);
+            cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *comp_intens_kernel_for_regs<vt,it,computational_intensity>), "getting function attributes (for # registers)", pass);
             if(!pass) return;
             this->register_usage = funcAttrib.numRegs;
 #ifdef DEBUG
