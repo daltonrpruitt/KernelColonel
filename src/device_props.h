3326eda2a33d4e17d6ea1e5a236432a3aaf548c0
daltonrpruitt <dp987@msstate.edu>
2022-02-17 14:01:12 -0600

    Add more useful error checking to dev_ctx init()

--- src/device_props.h
+++ src/device_props.h
@@ -2,7 +2,11 @@
 
 #include <local_cuda_utils.h>
 
+#include <iostream>
+
 using std::pow;
+using std::cout; 
+using std::endl;
 
 class device_context {
   public:
@@ -17,12 +21,14 @@ class device_context {
         cudaErrChk(cudaSetDevice(0), "finding GPU device", pass);
         if(pass) { cudaErrChk(cudaGetDeviceProperties(&props_, 0), "getting device properties", pass); }
         if(pass) { 
+            cout << "Successfully found GPU device "<< props_.name << endl;
             theoretical_bw_ = (float)props_.memoryClockRate  // kHz = kcycles/s
                             * props_.memoryBusWidth        // bit / cycle
                             / 8                                // byte / 8 bits
                             * 2                                // Assuming Double Data Rate (DDR) memory
                             * pow(1000.0, -2);                     // 1000 cycles/kcycle * 1 GB/10^9 B = 1/10^6
         }
+        cudaPrintLastError();
         return pass;
     }
 
