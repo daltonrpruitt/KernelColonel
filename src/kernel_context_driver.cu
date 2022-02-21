6833c4ccc1664a3f18c757e5ff6f2b137c5420a9
daltonrpruitt <dp987@msstate.edu>
2022-02-21 15:57:34 -0600

    Turn of other drivers and use new kernel

--- src/kernel_context_driver.cu
+++ src/kernel_context_driver.cu
@@ -8,6 +8,7 @@
 #include <copy.cu>
 #include <simple_indirection.cu>
 #include <overlap_index_access_with_data.cu>
+#include <overlap_index_switch.cu>
 #include <computation.cu>
 #include <output.h>
 #include <utils.h>
@@ -62,49 +63,53 @@ int main() {
         return -1;
     }
 
-    copy_driver_t copy_driver(N, bs_vec, output_dir+"copy_kernel_output.csv", &dev_ctx, true);
-    if (!copy_driver.check_then_run_kernels()) {return -1;} 
-    total_runs += copy_driver.get_total_runs();
+    // copy_driver_t copy_driver(N, bs_vec, output_dir+"copy_kernel_output.csv", &dev_ctx, true);
+    // if (!copy_driver.check_then_run_kernels()) {return -1;} 
+    // total_runs += copy_driver.get_total_runs();
 
     
-    indirection_driver_direct_t direct_driver(N, bs_vec,output_dir+"direct_kernel_output.csv", &dev_ctx, true);
-    if (!direct_driver.check_then_run_kernels()) {return -1;} 
-    indirection_driver_indirect_t indirect_driver(N, bs_vec, output_dir+"indirect_kernel_output.csv", &dev_ctx, true);
-    if (!indirect_driver.check_then_run_kernels()) {return -1;} 
-    total_runs += direct_driver.get_total_runs() + indirect_driver.get_total_runs();
+    // indirection_driver_direct_t direct_driver(N, bs_vec,output_dir+"direct_kernel_output.csv", &dev_ctx, true);
+    // if (!direct_driver.check_then_run_kernels()) {return -1;} 
+    // indirection_driver_indirect_t indirect_driver(N, bs_vec, output_dir+"indirect_kernel_output.csv", &dev_ctx, true);
+    // if (!indirect_driver.check_then_run_kernels()) {return -1;} 
+    // total_runs += direct_driver.get_total_runs() + indirect_driver.get_total_runs();
 
 
-    overlapped_access_driver_1_t overlapped_1_driver(N, bs_vec, output_dir+"overlapped_1_kernel_output.csv", &dev_ctx, true);
-    if (!overlapped_1_driver.check_then_run_kernels()) {return -1;} 
-    overlapped_access_driver_2_t overlapped_2_driver(N, bs_vec, output_dir+"overlapped_2_kernel_output.csv", &dev_ctx, true);
-    if (!overlapped_2_driver.check_then_run_kernels()) {return -1;} 
-    overlapped_access_driver_4_t overlapped_4_driver(N, bs_vec, output_dir+"overlapped_4_kernel_output.csv", &dev_ctx, true);
-    if (!overlapped_4_driver.check_then_run_kernels()) {return -1;} 
-    overlapped_access_driver_8_t overlapped_8_driver(N, bs_vec, output_dir+"overlapped_8_kernel_output.csv", &dev_ctx, true);
-    if (!overlapped_8_driver.check_then_run_kernels()) {return -1;} 
-    total_runs += overlapped_1_driver.get_total_runs() + overlapped_2_driver.get_total_runs() + 
-                            overlapped_4_driver.get_total_runs() + overlapped_8_driver.get_total_runs();
+    // overlapped_access_driver_1_t overlapped_1_driver(N, bs_vec, output_dir+"overlapped_1_kernel_output.csv", &dev_ctx, true);
+    // if (!overlapped_1_driver.check_then_run_kernels()) {return -1;} 
+    // overlapped_access_driver_2_t overlapped_2_driver(N, bs_vec, output_dir+"overlapped_2_kernel_output.csv", &dev_ctx, true);
+    // if (!overlapped_2_driver.check_then_run_kernels()) {return -1;} 
+    // overlapped_access_driver_4_t overlapped_4_driver(N, bs_vec, output_dir+"overlapped_4_kernel_output.csv", &dev_ctx, true);
+    // if (!overlapped_4_driver.check_then_run_kernels()) {return -1;} 
+    // overlapped_access_driver_8_t overlapped_8_driver(N, bs_vec, output_dir+"overlapped_8_kernel_output.csv", &dev_ctx, true);
+    // if (!overlapped_8_driver.check_then_run_kernels()) {return -1;} 
+    // total_runs += overlapped_1_driver.get_total_runs() + overlapped_2_driver.get_total_runs() + 
+    //                         overlapped_4_driver.get_total_runs() + overlapped_8_driver.get_total_runs();
 
 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 1>> comp_intens_1_driver(N, bs_vec, output_dir+"computational_intensity_1_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_1_driver.check_then_run_kernels()) {return -1;} 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 2>> comp_intens_2_driver(N, bs_vec, output_dir+"computational_intensity_2_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_2_driver.check_then_run_kernels()) {return -1;} 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 4>> comp_intens_4_driver(N, bs_vec, output_dir+"computational_intensity_4_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_4_driver.check_then_run_kernels()) {return -1;} 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 8>> comp_intens_8_driver(N, bs_vec, output_dir+"computational_intensity_8_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_8_driver.check_then_run_kernels()) {return -1;} 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 16>> comp_intens_16_driver(N, bs_vec, output_dir+"computational_intensity_16_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_16_driver.check_then_run_kernels()) {return -1;} 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 32>> comp_intens_32_driver(N, bs_vec, output_dir+"computational_intensity_32_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_32_driver.check_then_run_kernels()) {return -1;} 
-    MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 64>> comp_intens_64_driver(N, bs_vec, output_dir+"computational_intensity_64_kernel_output.csv", &dev_ctx, true);
-    if (!comp_intens_64_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 1>> comp_intens_1_driver(N, bs_vec, output_dir+"computational_intensity_1_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_1_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 2>> comp_intens_2_driver(N, bs_vec, output_dir+"computational_intensity_2_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_2_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 4>> comp_intens_4_driver(N, bs_vec, output_dir+"computational_intensity_4_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_4_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 8>> comp_intens_8_driver(N, bs_vec, output_dir+"computational_intensity_8_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_8_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 16>> comp_intens_16_driver(N, bs_vec, output_dir+"computational_intensity_16_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_16_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 32>> comp_intens_32_driver(N, bs_vec, output_dir+"computational_intensity_32_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_32_driver.check_then_run_kernels()) {return -1;} 
+    // MicrobenchmarkDriver<ComputationalIntensityContext<vt, int, 64>> comp_intens_64_driver(N, bs_vec, output_dir+"computational_intensity_64_kernel_output.csv", &dev_ctx, true);
+    // if (!comp_intens_64_driver.check_then_run_kernels()) {return -1;} 
 
-    total_runs += comp_intens_1_driver.get_total_runs() + comp_intens_2_driver.get_total_runs() + 
-                            comp_intens_4_driver.get_total_runs() + comp_intens_8_driver.get_total_runs() + 
-                            comp_intens_16_driver.get_total_runs() + comp_intens_32_driver.get_total_runs() + 
-                            comp_intens_64_driver.get_total_runs();
+    // total_runs += comp_intens_1_driver.get_total_runs(); //+ comp_intens_2_driver.get_total_runs() + 
+                            // comp_intens_4_driver.get_total_runs() + comp_intens_8_driver.get_total_runs() + 
+                            // comp_intens_16_driver.get_total_runs() + comp_intens_32_driver.get_total_runs() + 
+                            // comp_intens_64_driver.get_total_runs();
+
+    MicrobenchmarkDriver<OverlappedIndexSwitchKernel<vt, int, 1, 32>> overlapped_index_switch_driver(N, bs_vec, output_dir+"overlapped_index_switch_1_32_kernel_output.csv", &dev_ctx, true);
+    if (!overlapped_index_switch_driver.check_then_run_kernels()) {return -1;} 
+    total_runs += overlapped_index_switch_driver.get_total_runs();
 
 
     clock_gettime(CLOCK_MONOTONIC, &mainEnd);
