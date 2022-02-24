14398f73ace4d5d8d0432124225d6d3ea5a47f93
daltonrpruitt <dp987@msstate.edu>
2022-02-24 11:03:09 -0600

    Copy copy.cu as interleaved copy test
    For burst investigation

---
+++ src/kernels/burst_mode/interleaved_copy.cu
@@ -0,0 +1,120 @@
+#pragma once
+/**
+ * @file copy.cu
+ * @author Dalton Winans-Pruitt (daltonrpruitt@gmail.com)
+ * @brief Derived from KernelCPUContext; actual kernel specification
+ * @version 0.1
+ * @date 2022-01-27
+ * 
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include <cuda.h>
+#include <local_cuda_utils.h>
+#include <kernel_context.cu>
+
+#define DEBUG
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::vector;
+
+template<typename vt, typename it>
+__forceinline__ __host__ __device__        
+void kernel(uint idx, vt* gpu_in, vt* gpu_out){
+        gpu_out[idx] = gpu_in[idx];
+}
+
+
+template<typename vt, typename it>
+__global__        
+void kernel_for_regs(uint idx, vt* gpu_in, vt* gpu_out){
+        extern __shared__ int dummy[];
+        kernel<vt, it>(idx, gpu_in, gpu_out);
+}
+
+template<typename vt, typename it>
+struct ArrayCopyContext : public KernelCPUContext<vt, it> {
+    public:
+        typedef KernelCPUContext<vt, it> super;
+        // name = "Array_Copy";
+        int N = super::N;
+        int Gsz = super::Gsz;
+        int Bsz = super::Bsz;
+
+        vector<vt> & in = super::host_data[0];
+        vector<vt> & out = super::host_data[1];
+        vt* & d_in = super::device_data_ptrs[0];
+        vt* & d_out = super::device_data_ptrs[1];
+
+        int data_reads_per_element = 1;
+        int index_reads_per_element = 0;
+        int writes_per_element = 1;
+        struct gpu_ctx {
+            vt * gpu_in;
+            vt * gpu_out;   
+
+            __device__        
+            void operator() (uint idx){
+                extern __shared__ int dummy[];
+                kernel<vt, it>(idx, gpu_in, gpu_out);
+            }
+        } ctx ;
+
+        ArrayCopyContext(int n, int bs, device_context* dev_ctx, int shd_mem_alloc=0) 
+            : super(1, 1, 0, n, bs, dev_ctx, shd_mem_alloc) {
+            this->name = "ArrayCopy"; // _N=" +std::to_string(n) + "_Bs="+std::to_string(bs);
+            this->total_data_reads = N * data_reads_per_element;
+            this->total_index_reads = N * index_reads_per_element;
+            this->total_writes = N * writes_per_element;
+        }
+        ~ArrayCopyContext(){}
+
+        void init_inputs(bool& pass) override {
+            for(int i=0; i<N; ++i){
+                in.push_back(i);
+                out.push_back(0);
+            }
+        }
+
+        void init_indices(bool& pass) override {}
+
+        void set_dev_ptrs() override {
+            ctx.gpu_in = d_in;
+            ctx.gpu_out = d_out;
+        }
+
+        
+        void local_execute() override {
+            if(this->dev_ctx->props_.major >= 7) {
+                cudaFuncSetAttribute(compute_kernel<gpu_ctx>, cudaFuncAttributeMaxDynamicSharedMemorySize, this->dev_ctx->props_.sharedMemPerMultiprocessor);
+            }
+            compute_kernel<gpu_ctx><<<Gsz, Bsz, this->shared_memory_usage>>>(N, ctx);
+        }
+
+        bool local_check_result() override {
+            for(int i=0; i<N; ++i){
+                if(in[i] != out[i]){
+                    cout << "Validation Failed at " << i << ": in="<<in[i] << " out="<< out[i] << endl;
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        void local_compute_register_usage(bool& pass) override {   
+            // Kernel Registers 
+            struct cudaFuncAttributes funcAttrib;
+            cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *kernel_for_regs<vt,it>), "getting function attributes (for # registers)", pass);
+            if(!pass) {
+                this->okay = false; 
+                return;
+            }
+            this->register_usage = funcAttrib.numRegs;
+        }
+
+};
