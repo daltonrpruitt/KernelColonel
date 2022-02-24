c855f0c437ea66382ca88a757ee72daf4f05779f
daltonrpruitt <dp987@msstate.edu>
2022-02-24 11:17:22 -0600

    Starting updates to interleaved copy
    Name of struct, template params.

--- src/kernels/burst_mode/interleaved_copy.cu
+++ src/kernels/burst_mode/interleaved_copy.cu
@@ -21,7 +21,7 @@ using std::cout;
 using std::endl;
 using std::vector;
 
-template<typename vt, typename it>
+template<typename vt, typename it, int local_group_size, int elements>
 __forceinline__ __host__ __device__        
 void kernel(uint idx, vt* gpu_in, vt* gpu_out){
         gpu_out[idx] = gpu_in[idx];
@@ -35,8 +35,8 @@ void kernel_for_regs(uint idx, vt* gpu_in, vt* gpu_out){
         kernel<vt, it>(idx, gpu_in, gpu_out);
 }
 
-template<typename vt, typename it>
-struct ArrayCopyContext : public KernelCPUContext<vt, it> {
+template<typename vt, typename it, int local_group_size, int elements>
+struct InterleavedCopyContext : public KernelCPUContext<vt, it> {
     public:
         typedef KernelCPUContext<vt, it> super;
         // name = "Array_Copy";
@@ -59,18 +59,18 @@ struct ArrayCopyContext : public KernelCPUContext<vt, it> {
             __device__        
             void operator() (uint idx){
                 extern __shared__ int dummy[];
-                kernel<vt, it>(idx, gpu_in, gpu_out);
+                kernel<vt, it, local_group_size, elements>(idx, gpu_in, gpu_out);
             }
         } ctx ;
 
-        ArrayCopyContext(int n, int bs, device_context* dev_ctx, int shd_mem_alloc=0) 
+        InterleavedCopyContext(int n, int bs, device_context* dev_ctx, int shd_mem_alloc=0) 
             : super(1, 1, 0, n, bs, dev_ctx, shd_mem_alloc) {
-            this->name = "ArrayCopy"; // _N=" +std::to_string(n) + "_Bs="+std::to_string(bs);
+            this->name = "InterleavedCopy"; 
             this->total_data_reads = N * data_reads_per_element;
             this->total_index_reads = N * index_reads_per_element;
             this->total_writes = N * writes_per_element;
         }
-        ~ArrayCopyContext(){}
+        ~InterleavedCopyContext(){}
 
         void init_inputs(bool& pass) override {
             for(int i=0; i<N; ++i){
@@ -107,7 +107,7 @@ struct ArrayCopyContext : public KernelCPUContext<vt, it> {
         void local_compute_register_usage(bool& pass) override {   
             // Kernel Registers 
             struct cudaFuncAttributes funcAttrib;
-            cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *kernel_for_regs<vt,it>), "getting function attributes (for # registers)", pass);
+            cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *kernel_for_regs<vt,it,local_group_size,elements>), "getting function attributes (for # registers)", pass);
             if(!pass) {
                 this->okay = false; 
                 return;
