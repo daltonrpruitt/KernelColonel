56fcc43fb8444258ebbf650e8f500607c94399a6
daltonrpruitt <dp987@msstate.edu>
2022-03-14 09:15:35 -0500

    Add "first try" of output data plotting
    This is a fairly well-working general plotting algorithm.
    I have some changes to make (such as plotting unique combos of configs
    on a single plot), but this seems to be sufficient in structure.

--- src/postprocessing/plot_general.py
+++ src/postprocessing/plot_general.py
@@ -95,6 +95,54 @@ def plot_general(all_data, kernel_name, x_field, y_field, fields_to_keep_constan
             print("Stopping current plotting...")
             return
 
+    multi_plot_field_uniques = data[field_for_multiplotting].unique()
+
+    colors = plt.cm.rainbow(np.linspace(0, 1, len(multi_plot_field_uniques)))
+
+    for i, constants in enumerate(unique_combos):
+        title_configs = ""
+        filename_configs = ""
+        for j in range(len(fields_to_keep_constant)):
+            title_configs += fields_to_keep_constant[j]+"="+str(constants[j]) + (", " if j < len(fields_to_keep_constant) - 1 else "")
+            filename_configs += fields_to_keep_constant[j]+"-"+str(constants[j]) + ("_" if j < len(fields_to_keep_constant) - 1 else "")
+        filename = filename_base + "_" + filename_configs
+        ic(title_configs)
+
+        plt.close("all")
+        
+        # Filter data to only that matching the corrent constants
+        multi_local_data = data.copy(deep=False)
+        for j in range(len(fields_to_keep_constant)):
+            multi_local_data = multi_local_data.loc[multi_local_data[fields_to_keep_constant[j]] == constants[j]]
+    
+        # Work through and plot each unique multiplot trend
+        for j, individual_val in enumerate(multi_plot_field_uniques): 
+            print(individual_val)
+
+            fig = plt.figure()
+            fig.suptitle(f"{plot_title_base} ({date_time_str})\n{title_configs}", fontsize=11)
+            plt.xlabel(field_strings[x_field] if x_field in field_strings.keys() else x_field)
+            plt.ylabel(field_strings[y_field] if y_field in field_strings.keys() else y_field)
+
+            if x_field in field_bounds: plt.xlim(field_bounds[x_field])
+            if y_field in field_bounds: plt.ylim(field_bounds[y_field])
+
+            plot_local_data = multi_local_data.loc[multi_local_data[field_for_multiplotting] == individual_val]
+            
+            ic(plot_local_data)
+            plt.plot(plot_local_data[x_field], 
+                     plot_local_data[y_field], 
+                     c=colors[j], 
+                     marker="o",
+                     label=(
+                        (field_strings[field_for_multiplotting] if field_for_multiplotting in field_strings.keys() else field_for_multiplotting )
+                        +"="+str(individual_val)
+                        )
+            )
+    
+        plt.legend(loc="best")
+        plt.savefig(os.path.join(images_dir, filename) + ".png")
+        plt.close()
 
 for p in plot_configs:
     kernel = p[0]
