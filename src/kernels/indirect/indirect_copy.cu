41f0d1124199c7672fcb9562488786e1adab94b1
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2022-03-20 21:05:52 -0500

    Add output sampling to indirect copy kernel check
    Similar to that in some other kernels, this will output on failure. 
    But I also want to just look at a sample of what is being output to confirm 
    it is at least what I think it is....

--- src/kernels/indirect/indirect_copy.cu
+++ src/kernels/indirect/indirect_copy.cu
@@ -141,10 +141,26 @@ struct IndirectCopyContext : public KernelCPUContext<vt, it> {
         bool local_check_result() override {
             for(int i=0; i<N; ++i){
                 if(in[indices[i]] != out[i]){
-                    cout << "Validation Failed at " << i << ": in="<<in[i] << " out="<< out[i] << endl;
+                    cout << "Validation Failed at " << i << ": in="<<in[i] << " idx=" << indices[i] << " out="<< out[i] << endl;
+                    
+                    cout << "Debug dump of in and out array: " << endl;
+                    cout << std::setw(10) << "IN" << "  |" << std::setw(10) << "IDX" << "  |" << std::setw(10) << "OUT" << endl; 
+                    int output_size = 20;
+                    unsigned long long j = max((int)0, (int)(i - output_size/2));
+                    for(int k=0; k < output_size; ++k, ++j) { 
+                        cout << std::setw(10) << in[j] << "  |" << std::setw(10) << indices[j] <<"  |" << std::setw(10) << out[j] << endl; 
+                    }      
                     return false;
                 }
             }
+#ifdef DEBUG
+            for(int i=0; i < shuffle_size*3; i+=shuffle_size/4) {
+                for(int j=0; j < 5; ++j) {
+                    unsigned long long curr_idx = k-2+j;
+                    cout << std::setw(10) << in[curr_idx] << "  |" << std::setw(10) << indices[curr_idx] <<"  |" << std::setw(10) << out[curr_idx] << endl; 
+                }
+            }      
+#endif
             return true;
         }
 
