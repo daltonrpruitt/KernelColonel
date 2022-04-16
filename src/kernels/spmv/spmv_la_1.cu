b8cc485eb70adb1d556479440b97528ea7af42c7
Dalton Pruitt <dp987@msstate.edu>
2022-04-16 11:01:32 -0500

    Add switch to include preload arithmatic
    Want to see the impact of not including the preload arithmatic if not
    doing preload.
    So, really, there are just three cases for preloading switches:
    - preloading (with arithmatic)
    - not preloading with arithmatic
    - not preloading without arithmatic
    This is reflected by the `static_assert()` in the constructor.

--- src/kernels/spmv/spmv_la_1.cu
+++ src/kernels/spmv/spmv_la_1.cu
@@ -56,7 +56,7 @@ void force_global_load(vt* arr, uint offset, uint m) {
 #define MAX_THREADS_PER_BLOCK 64
 #define MIN_BLOCKS_PER_MP     2
 
-template <typename it=int, typename vt=double, bool preload, int chunk_parts>
+template <typename it=int, typename vt=double, bool preload=false, bool include_preload_arith=false, int chunk_parts=1>
 __launch_bounds__(MAX_THREADS_PER_BLOCK, MIN_BLOCKS_PER_MP)
 // __forceinline__ __host__ __device__ 
 __global__ 
@@ -87,14 +87,18 @@ void spmv_kernel_latency_amortization_1(vt* product, CRSMat_gpu matrix, vt* vec)
     for(int chunk=0; chunk < num_chunks; chunk++) {
         uint local_start = start + chunk * chunk_size;
 
-        uint local_start_col_idx = matrix.indices[local_start];
-        uint local_stop_col_idx = max(matrix.indices[min(local_start + chunk_size, stop)-1], local_start_col_idx + 1);
-        uint cur_preload_start_idx = local_start_col_idx;
-        while(cur_preload_start_idx < local_stop_col_idx) {
-            if constexpr(preload){
-                force_global_load<vt>(vec, cur_preload_start_idx + lane*stride, local_stop_col_idx);
+        if constexpr(include_preload_arith || preload) {
+            uint local_start_col_idx = matrix.indices[local_start];
+            uint local_stop_col_idx = max(matrix.indices[min(local_start + chunk_size, stop)-1], local_start_col_idx + 1);
+            uint cur_preload_start_idx = local_start_col_idx;
+        }
+        if constexpr(include_preload_arith || preload) {
+            while(cur_preload_start_idx < local_stop_col_idx) {
+                if constexpr(preload){
+                    force_global_load<vt>(vec, cur_preload_start_idx + lane*stride, local_stop_col_idx);
+                }
+                cur_preload_start_idx += WARP_SIZE * stride;
             }
-            cur_preload_start_idx += WARP_SIZE * stride;
         }
     
     
@@ -119,7 +123,7 @@ void spmv_kernel_latency_amortization_1(vt* product, CRSMat_gpu matrix, vt* vec)
     return;
 }
 
-template <typename it=int, typename vt=double, bool preload=false, int chunk_parts=1>
+template <typename it=int, typename vt=double, bool preload=false, bool include_preload_arith=false, int chunk_parts=1>
 struct SpmvKernelLAv1 : SpmvKernel<it, vt> {
    public:
     typedef SpmvKernel<it, vt> super;
@@ -128,6 +132,7 @@ struct SpmvKernelLAv1 : SpmvKernel<it, vt> {
     SpmvKernelLAv1(int bs, device_context* d_ctx, int shd_mem_alloc = 0, int matrix_file_id=0) 
     : super(bs, d_ctx, shd_mem_alloc, matrix_file_id) {
         this->name = "SpmvKernelLAv1";
+        static_assert(!( preload && !include_preload_arith)); // if preload, must include arith (just to reduce test space complexity)
     }
     ~SpmvKernelLAv1() {}
 
@@ -142,10 +147,15 @@ struct SpmvKernelLAv1 : SpmvKernel<it, vt> {
     float local_execute() override {  
         if(this->dev_ctx->props_.major >= 7) {
             cudaFuncAttributes attr;
-            cudaFuncGetAttributes(&attr, (void *) spmv_kernel_latency_amortization_1<int, double>);
+            cudaFuncGetAttributes(&attr, 
+                (void *) spmv_kernel_latency_amortization_1<int, double, preload, include_preload_arith, chunk_parts>);
             int shmem = this->dev_ctx->props_.sharedMemPerMultiprocessor-1024-attr.sharedSizeBytes;
-            cudaFuncSetAttribute((void *) spmv_kernel_latency_amortization_1<int, double>, cudaFuncAttributeMaxDynamicSharedMemorySize, shmem);
-            cudaFuncSetAttribute((void *) spmv_kernel_latency_amortization_1<int, double>, cudaFuncAttributePreferredSharedMemoryCarveout, cudaSharedmemCarveoutMaxShared);
+            cudaFuncSetAttribute(
+                (void *) spmv_kernel_latency_amortization_1<int, double, preload, include_preload_arith, chunk_parts>, 
+                cudaFuncAttributeMaxDynamicSharedMemorySize, shmem);
+            cudaFuncSetAttribute(
+                (void *) spmv_kernel_latency_amortization_1<int, double, preload, include_preload_arith, chunk_parts>, 
+                cudaFuncAttributePreferredSharedMemoryCarveout, cudaSharedmemCarveoutMaxShared);
             cudaPrintLastError();
         }
         cudaEvent_t start, stop;
@@ -156,7 +166,7 @@ struct SpmvKernelLAv1 : SpmvKernel<it, vt> {
         // dense_vector_cache_preload<<<preload_blocks, Bsz, shared_memory_usage>>>(gpu_vector, gpu_matrix.m);
         // cudaDeviceSynchronize();
         // cudaPrintLastError();
-        spmv_kernel_latency_amortization_1<it,vt,preload,chunk_parts>
+        spmv_kernel_latency_amortization_1<it,vt,preload,include_preload_arith,chunk_parts>
             <<<this->Gsz, this->Bsz, this->shared_memory_usage>>>(this->gpu_results, this->gpu_matrix, this->gpu_vector);
         cudaEventRecord(stop);
 
@@ -174,7 +184,7 @@ struct SpmvKernelLAv1 : SpmvKernel<it, vt> {
     void local_compute_register_usage(bool& pass) override {
         // Kernel Registers
         struct cudaFuncAttributes funcAttrib;
-        cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *spmv_kernel_latency_amortization_1<it,vt,preload,chunk_parts>), "getting function attributes (for # registers)", pass);
+        cudaErrChk(cudaFuncGetAttributes(&funcAttrib, *spmv_kernel_latency_amortization_1<it,vt,preload,include_preload_arith,chunk_parts>), "getting function attributes (for # registers)", pass);
         if (!pass) {
             this->okay = false;
             return;
