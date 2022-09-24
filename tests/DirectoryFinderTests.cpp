656cd85178089d46c951930778643c1131e8fd8f
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2022-09-23 21:40:19 -0500

    Heavily update DirFinderTests for 1st actual test
    Not a great test, but should help.
    The Output/DirectoryFinder class must be heavily changed to fit!
    But will be general at the end of this process. Nice.

--- tests/DirectoryFinderTests.cpp
+++ tests/DirectoryFinderTests.cpp
@@ -1,10 +1,65 @@
+
+#include "output.h"
+
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <filesystem>
+#include <iostream>
 
 
-// Demonstrate some basic assertions.
-TEST(TestTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+namespace KernelColonel {
+
+namespace utilities {
+
+
+namespace fs = std::filesystem;
+using ::testing::HasSubstr;
+
+class DirectoryFinderTest : public ::testing::Test {
+  protected:
+   DirectoryFinderTest() {}
+
+   ~DirectoryFinderTest() override {}
+
+   void SetUp() override {
+      ASSERT_THAT(fs::current_path().string(), HasSubstr("KernelColonel"));
+   }
+
+   void TearDown() override {}
+};
+
+TEST_F(DirectoryFinderTest, FailsToFind) {
+  
+    // Arrange
+    
+    // Act
+        EXPECT_THROW({
+        try
+        {
+            DirectoryFinder(std::vector<std::string>("non_existant_directory"), "should_not_create");
+        }
+        catch( const DirectoryFinderException& e )
+        {
+            // and this tests that it has the correct message
+            EXPECT_STREQ( "Sibling directories could not be found", e.what() );
+            throw;
+        }
+    }, DirectoryFinderException );
+    
+    // Assert  
+    ASSERT_FALSE(std::filesystem::exists("non_existant_directory"));
+    // probably not a great assertion, but oh well.
 }
+/*
+TEST(DirectoryFinderTest, BasicAssertions) {
+
+}
+
+TEST(DirectoryFinderTest, BasicAssertions) {
+
+}
+*/
+} // namespace utilities
+
+} // namespace KernelColonel
