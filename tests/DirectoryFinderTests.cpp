36af32387d226adaceff3f521a297114316731aa
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2022-09-24 21:10:38 -0500

    Add useful setup/teardown for directory stuff
    This turns out to be pretty neat when running the test from different
    starting directories.

--- tests/DirectoryFinderTests.cpp
+++ tests/DirectoryFinderTests.cpp
@@ -4,6 +4,8 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
+#include <string>
+#include <string_view>
 #include <filesystem>
 #include <iostream>
 
@@ -12,7 +14,7 @@ namespace KernelColonel {
 
 namespace utilities {
 
-
+constexpr std::string_view PROJECT_BASE_DIR {"KernelColonel"};
 namespace fs = std::filesystem;
 using ::testing::HasSubstr;
 
@@ -20,13 +22,34 @@ class DirectoryFinderTest : public ::testing::Test {
   protected:
    DirectoryFinderTest() {}
 
-   ~DirectoryFinderTest() override {}
+   ~DirectoryFinderTest() override {
+        if (fs::exists(starting_working_dir)) fs::current_path(starting_working_dir);
+    }
 
+    // will create subdirectories "./parent/child" and move to "child" as starting point
    void SetUp() override {
-      ASSERT_THAT(fs::current_path().string(), HasSubstr("KernelColonel"));
-   }
+        ASSERT_THAT(starting_working_dir.string(), HasSubstr(PROJECT_BASE_DIR));
+        while(fs::current_path().parent_path().string().find(PROJECT_BASE_DIR) != std::string::npos) {
+            fs::current_path(fs::current_path().parent_path());
+        }
+        fs::current_path(fs::current_path() / "build" / "tests" / "Debug");
+        if ( ! fs::exists(dummy_parent_dir)) fs::create_directory(dummy_parent_dir);
+        if ( ! fs::exists(dummy_child_dir)) fs::create_directory(dummy_child_dir);
+        ASSERT_TRUE(fs::exists(dummy_child_dir));
+        fs::current_path(dummy_child_dir);
 
-   void TearDown() override {}
+    }
+
+    void TearDown() override {
+        fs::current_path(starting_working_dir);
+        ASSERT_EQ(fs::current_path(), starting_working_dir);
+    }
+
+    std::string dummy_parent_dir_name = "test_parent_directory";
+    std::string dummy_child_dir_name = "test_child_directory";
+    fs::path starting_working_dir = fs::current_path();
+    fs::path dummy_parent_dir = starting_working_dir / fs::path(dummy_parent_dir_name);
+    fs::path dummy_child_dir = dummy_parent_dir / fs::path(dummy_child_dir_name);
 };
 
 TEST_F(DirectoryFinderTest, FailToFindParent) {
