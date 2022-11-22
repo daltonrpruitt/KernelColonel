64e173ddfde3744a55a0453002e2298d834a1d5e
Dalton Winans-Pruitt <daltonrpruitt@gmail.com>
2022-11-22 16:20:03 -0600

    Copied IKernelData header for implementation file

---
+++ src/cpp/IKernelData.cu
@@ -0,0 +1,206 @@
+/**
+ * @file IKernelData.cu
+ * @author Dalton Winans-Pruitt (daltonrpruitt@gmail.com)
+ * @brief Implements 
+ * 
+ */
+
+#include <utils/local_cuda_utils.h>
+
+#include <vector>
+#include <algorithm>
+
+#include <cuda.h>
+
+using std::string;
+using std::to_string;
+using std::cout;
+using std::endl;
+using std::vector;
+
+
+
+/**
+ * @brief Container for CPU/GPU data for different situations; used for reducing `cudaMemCpy()` calls
+ * 
+ * Necessary because different kernel contexts may use the same input data, but will the data
+ * will be removed and re-computed and copied between separate kernel context instance executions.
+ * 
+ * @tparam value_t Value type (data arrays)
+ * @tparam it Index type (indirection arrays)
+ */
+template<typename value_t, 
+         typename index_t, 
+         unsigned int num_in_data, 
+         unsigned num_out_data, 
+         unsigned int num_indices>
+class IKernelData { 
+  public:
+    IKernelData(unsigned long long n) : N(n) {}
+
+    ~IKernelData(){
+        uninit();            
+    }
+
+    /**
+     * @brief Setup all CPU and GPU data/index arrays
+     * 
+     * Setup on CPU side, allocate GPU memory, copy data over to GPU. 
+     * 
+     * @return true Prematurely return if already initialized
+     * @return false Failed to initialize properly (handling taken care of by owner of object)
+     */
+    bool init(int dev_ctx_id){
+        if(initialized) {
+            if(dev_ctx_id == gpu_device_id) {
+                cout <<"Tried to reinitialize "<< this->name << "with same device id=" << gpu_device_id << "; Ignored" << std::endl;
+                return true;
+            } else {
+                cerr<<"Tried to reinitialize "<< this->name << "with current device id=" << gpu_device_id <<" and new device ID="<<dev_ctx_id << "; Not allowed!" << std::endl;
+                return false;
+            }
+        }
+        gpu_device_id = dev_ctx_id;
+
+        bool pass = true;
+        if(input_size == 0) { input_size = N; }
+        if(output_size == 0) { output_size = N; }
+        if(indices_size == 0) { indices_size = N; }
+
+        if(pass) initInputsCpu();
+        if(pass) initIndicesCpu();
+
+        // init inputs/indices gpu...
+        if(pass){
+            cudaErrChk(cudaSetDevice(gpu_device_id), "setting device " + std::to_string(gpu_device_id));
+            for(int i=0; i < num_in_data; ++i) {
+                cudaErrChk(cudaMalloc((void **)&device_data_ptrs[i], input_size * sizeof(value_t)),"device_data_ptrs["+to_string(i)+"] mem allocation", pass);
+                if(!pass) break;
+            }
+
+            if(pass) {
+                for(int i=num_in_data; i < num_total_data; ++i) {
+                    cudaErrChk(cudaMalloc((void **)&device_data_ptrs[i], output_size * sizeof(value_t)),"device_data_ptrs["+to_string(i)+"] mem allocation", pass);
+                    if(!pass) break;
+                }
+            }
+            
+            if(pass) {
+                for(int i=0; i < num_in_data; ++i) {
+                    cudaErrChk(cudaMemcpy(device_data_ptrs[i], host_data[i].data(), input_size * sizeof(value_t), cudaMemcpyHostToDevice), "copy host_data["+to_string(i)+"] to device_data_ptrs["+to_string(i)+"]", pass);                
+                    if(!pass) break;
+                }
+            }
+
+            for(int i=0; i < num_indices; ++i) {
+                cudaErrChk(cudaMalloc((void **)&device_indices_ptrs[i], indices_size * index_t_size),"device_indices_ptrs["+to_string(i)+"] mem allocation", pass);
+                if(!pass) break;
+            }
+
+            if(pass) {
+                for(int i=0; i < num_indices; ++i) {
+                    cudaErrChk(cudaMemcpy(device_indices_ptrs[i], host_indices[i].data(), indices_size * index_t_size, cudaMemcpyHostToDevice), "copy host_indices["+to_string(i)+"] to device_indices_ptrs["+to_string(i)+"]", pass);                
+                    if(!pass) break;
+                }
+            }
+
+            if(pass) { setGpuNamedData(); }
+        }
+
+        if(!pass) {
+            freeGpuData();
+            freeCpuData();
+            okay = false;
+            cerr<<"Error in initializing "<<this->name << "for N="<<this->N;
+            if(input_size != 0) cout << " input_sz="<<input_size;
+            if(output_size != 0) cout << " output_sz="<<output_size;
+            if(indices_size != 0) cout << " indices_sz="<<indices_size;
+            cerr << " !" << endl;
+        }
+        if(pass) initialized = true;
+        return pass;
+    }
+
+    /**
+     * @brief Free relevant structures (CPU and GPU)
+     */
+    void uninit() {
+        if(!initialized) {
+            std::cout << "Attempted to uninit() a KernelData instance without being init()'d!" << std::endl;
+            return;
+        }
+        freeGpuData();
+        freeCpuData();
+        initialized = false;
+    }
+    
+    void copyOutputToDevice() {
+        for(int i=num_in_data; i < num_total_data; ++i) {
+            cudaErrChk(cudaMemcpy(host_data[i].data(), device_data_ptrs[i], output_size * sizeof(value_t), cudaMemcpyDeviceToHost),"copying device_data_ptrs["+to_string(i)+"] to host_data["+to_string(i)+"]");
+        }            
+    }
+
+  private: 
+    /**
+     * @brief Placeholder for user-defined input data array(s) initialization
+     * 
+     * Throws an exception if fails.
+     */
+    virtual void initInputsCpu() = 0;
+
+    /**
+     * @brief Placeholder for user-defined indices array(s) initialization
+     * 
+     * Throws an exception if fails.
+     */
+    virtual void initIndicesCpu() = 0;
+
+    /**
+     * @brief Placeholder for user-defined gpu data structure initialization
+     * 
+     * Simple aliasing of pointers, so should not fail.
+     */
+     virtual void setGpuNamedData() = 0;
+
+    /**
+     * @brief Free GPU memory
+     */
+    void freeGpuData(){
+        cudaSetDevice(gpu_device_id);
+        for(value_t* ptr : device_data_ptrs)     { cudaFree(ptr); ptr = nullptr; }
+        for(index_t* ptr : device_indices_ptrs)  { cudaFree(ptr); ptr = nullptr; }
+    }
+          
+    /**
+     * @brief Free CPU memory
+     */
+    void freeCpuData(){
+        for(int i=0; i<num_total_data; ++i) { 
+            vector<value_t>().swap(host_data[i]); 
+        }
+        for(int i=0; i<num_indices; ++i) { 
+            vector<index_t>().swap(host_indices[i]); 
+        }
+    }
+
+    static constexpr unsigned int value_t_size = sizeof(value_t);
+    static constexpr unsigned int index_t_size = sizeof(index_t);
+    static constexpr unsigned int num_total_data = num_in_data + num_out_data;
+    string name;
+    
+    bool okay = true;
+    bool initialized = false;
+    
+    int gpu_device_id;
+protected:
+    unsigned long long N=0;
+    unsigned long long input_size=0;
+    unsigned long long output_size=0;
+    unsigned long long indices_size=0;
+    vector<vector<value_t>> host_data{(unsigned long)num_total_data};
+    vector<value_t *> device_data_ptrs{(unsigned long)num_total_data};
+    
+    vector<vector<index_t>> host_indices{(unsigned long)num_indices};
+    vector<index_t *> device_indices_ptrs{(unsigned long)num_indices};
+
+};
