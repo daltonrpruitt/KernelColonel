1cc1d71e99cb4147c05adf0fdc71d7a4a28f8a42
Dalton Winans-Pruitt <daltonrpruitt@gmail.com>
2022-11-26 16:29:46 -0600

    Add replaceAll string utility from internet

--- src/include/utils/utils.hpp
+++ src/include/utils/utils.hpp
@@ -10,6 +10,7 @@
  */
 
 #include <time.h>
+#include <algorithm>
 #include <string>
 #include <iostream>
 
@@ -21,4 +22,19 @@ double elapsed_time_ms(timespec startTime, timespec endTime){
 std::string bool_to_string(bool b){
     // Python capitalization style
     return b ? "True" : "False";
+}
+
+/**
+ * @brief From https://stackoverflow.com/a/24315631
+ * 
+ * @param str String we are modifying
+ * @param from String to remove
+ * @param to String to put in
+ */
+static inline void replaceAll(std::string& str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length(); // Handles case where 'to' is a substring of 'from'
+    }
 }
