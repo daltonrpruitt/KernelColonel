6716854cf6540b8b02150b87ed54e41b250e65b5
Dalton Winans-Pruitt <daltonrpruitt@gmail.com>
2023-04-23 21:19:01 -0500

    Use new class in `main()`
    Just rework recent json "test".
    Need to add real tests later.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,13 +1,12 @@
-#include <fstream>
 #include <iostream>
-#include <nlohmann/json.hpp>
+
+#include <ConfigStore.hpp>
 
 int main(int argc, char **argv) {
     assert(argc == 2);
     try {
-        std::ifstream file(argv[1]);
-        auto config = nlohmann::json::parse(file, nullptr, true, true);
-        std::cout << config.dump(4) << std::endl;
+        KernelColonel::ConfigStore::fromFile(argv[1]);
+        std::cout << KernelColonel::ConfigStore::getConfig().dump(4) << std::endl;
     } catch (const std::exception &e) {
         std::cout << e.what() << std::endl;
     }
