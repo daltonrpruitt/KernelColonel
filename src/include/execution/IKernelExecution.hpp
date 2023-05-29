cc9b16c2785cdea6a06cafea020d26e6b771fd9d
Dalton Winans-Pruitt <daltonrpruitt@gmail.com>
2023-05-29 16:48:35 -0500

    Simplify `IKernelExecution` class (start)

--- src/include/execution/IKernelExecution.hpp
+++ src/include/execution/IKernelExecution.hpp
@@ -26,7 +26,7 @@ namespace KernelColonel {
  * @tparam value_t Value type (data arrays)
  * @tparam it Index type (indirection arrays)
  */
-template<class gpu_data_struct_t, type kernel_data_t>
+template<type kernel_data_t>
 class IKernelExecution { 
   public:
     IKernelExecution(unsigned long long n);
@@ -57,66 +57,15 @@ class IKernelExecution {
     bool check(std::shared_ptr<kernel_data_t> data);
 
     /**
-     * @brief Free relevant structures (CPU and GPU)
+     * @brief Execute a run once and give the GPU execution time
+     * 
+     * @return double Execution time of a single run (GPU time only) 
      */
-    void uninit();
-    
-    void copyOutputFromDeviceToHost();
+    double time_single_execution();
 
   private: 
-    /**
-     * @brief Placeholder for user-defined input data array(s) initialization
-     * 
-     * Throws an exception if fails.
-     */
-    virtual void initInputsCpu() = 0;
-
-    /**
-     * @brief Placeholder for user-defined indices array(s) initialization
-     * 
-     * Throws an exception if fails.
-     */
-    virtual void initIndicesCpu() = 0;
-
-    /**
-     * @brief Placeholder for user-defined gpu data structure initialization
-     * 
-     * Simple aliasing of pointers, so should not fail.
-     */
-     virtual void setGpuNamedData() = 0;
-
-    /**
-     * @brief Free GPU memory
-     */
-    void freeGpuData();
-          
-    /**
-     * @brief Free CPU memory
-     */
-    void freeCpuData();
-
-    static constexpr unsigned int value_t_size = sizeof(value_t);
-    static constexpr unsigned int index_t_size = sizeof(index_t);
-    static constexpr unsigned int num_total_data = num_in_data + num_out_data;
     std::string name;
-    
-    bool okay = true;
-    DataState state = DataState::PREINIT;
-    
-    int gpu_device_id;
 protected:
-    unsigned long long N=0;
-    unsigned long long input_size=0;
-    unsigned long long output_size=0;
-    unsigned long long indices_size=0;
-
-    std::vector<std::vector<value_t>> host_data{(unsigned long)num_total_data};
-    std::vector<value_t *> device_data_ptrs{(unsigned long)num_total_data};
-    
-    std::vector<std::vector<index_t>> host_indices{(unsigned long)num_indices};
-    std::vector<index_t *> device_indices_ptrs{(unsigned long)num_indices};
-
-    gpu_data_s_t gpu_named_data;
 };
 
 } // namespace KernelColonel
