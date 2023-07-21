4345e5574063a90c161a844c16ab6f63b25f899b
Dalton P <58988139+daltonrpruitt@users.noreply.github.com>
2023-07-20 20:50:03 -0500

    Add test for the SimpleExec on diff data types
    Seems to work for now.
    WARNING: cannot use `unsigned long long` (or probably `long long`
    in general); I think CUDA doesn't support it?
    It may be over 64bits on my system?

--- tests/cpp/SimpleKernelExecutionTests.cu
+++ tests/cpp/SimpleKernelExecutionTests.cu
@@ -122,3 +122,43 @@ TEST_F(SimpleKernelExecutionTests, ExecuteOnData)
     std::cout << "Executing " << copy_kernel_program_name << " with grid=" << grid << " and block="<<block << " took " << time << "ms" << std::endl;
 }
 
+/**
+ * @brief Helper function to repeatedly test SimpleKernelExecution::time_single_execution()
+ */
+template<typename value_type>
+void executeCopyKernelOnNewData(unsigned long long data_size, dim3 grid, dim3 block, std::string type_string, std::string copy_kernel_program_name, jitify::Program& program)
+{
+    auto check_lambda = [&](const auto& input, const auto& output, const auto& indices)
+    {
+        if (input.size() != output.size())
+            return false;
+        for (int i = 0; i < input.size(); i++)
+            if (input[i] != output[i])
+            {
+                return false;
+            }
+        return true;
+    };
+
+    auto new_data = std::make_shared<kc::SimpleKernelData<value_type>>(data_size);
+    new_data->setInitInputsFunc(whole_numbers<value_type>);
+    kc::SimpleKernelExecution<value_type> new_exec(copy_kernel_program_name, program, check_lambda);
+
+    auto time = new_exec.time_single_execution(new_data, grid, block);
+    ASSERT_TRUE(new_exec.check(new_data));
+    std::cout << "Executing " << copy_kernel_program_name << " vt="<<type_string<< " with grid = " << grid << " and block = " << block << " took " << time << "ms" << std::endl;
+}
+
+TEST_F(SimpleKernelExecutionTests, ExecutionOnDifferentDataTypes)
+{
+
+    using jitify::reflection::reflect_template;
+
+    dim3 grid(1);
+    dim3 block(1);
+
+    executeCopyKernelOnNewData<int>(data_size, grid, block, "int", copy_kernel_program_name, program);
+    executeCopyKernelOnNewData<float>(data_size, grid, block, "float", copy_kernel_program_name, program);
+    executeCopyKernelOnNewData<unsigned long>(data_size, grid, block, "unsigned long", copy_kernel_program_name, program);
+}
+
